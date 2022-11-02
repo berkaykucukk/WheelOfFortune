24c966fae9ae31006b136d5761d4fbcdc9a727d7
berkay <berkaykucuk1905@gmail.com>
2022-11-02 18:55:05 +0300

    unity elements

---
+++ Assets/_GameAssets/Scripts/Controllers/Animation/UIElementAnimationController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+
+public class UIElementAnimationController : MonoBehaviour
+{
+    #region INSPECTOR PROPERTIES
+
+    #endregion
+}
