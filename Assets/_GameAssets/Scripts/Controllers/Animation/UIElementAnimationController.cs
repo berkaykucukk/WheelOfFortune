08392a31152e2331afad398bf0ffb81c1a7b962c
berkay <berkaykucuk1905@gmail.com>
2022-11-04 16:03:51 +0300

    animation ok

--- Assets/_GameAssets/Scripts/Controllers/Animation/UIElementAnimationController.cs
+++ Assets/_GameAssets/Scripts/Controllers/Animation/UIElementAnimationController.cs
@@ -3,18 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(WheelOfFortuneEventsListener))]
 public class UIElementAnimationController : MonoBehaviour
 {
     #region PRIVATE PROPERTIES
 
-    private float spinWheelAngle = 360f;
-    private int _numberOfSpinWheelItems;
-    private float _durationSpin;
-    private int _numberRotate;
-    private AnimationCurve _curveSpin;
+    private float SpinWheelAngle = 360f;
     private WheelOfFortuneEventsListener EventsListener => GetComponent<WheelOfFortuneEventsListener>();
+    private float spinTimer = 0;
+    private float angleSection;
 
     #endregion
 
@@ -23,25 +22,38 @@ public class UIElementAnimationController : MonoBehaviour
 
     private void OnEnable()
     {
-        EventsListener.onSpinReady += SpinWheel;
+        EventsListener.onSpinReady += RunSpinWheel;
+    }
+
+    private void OnDisable()
+    {
+        EventsListener.onSpinReady -= RunSpinWheel;
     }
 
     #endregion
 
-    private void SpinWheel(int numberOfItems, float duration, int numberRotate,
+    private void RunSpinWheel(int numberOfItems, float durationRotate, int numberRotate,
         AnimationCurve curveSpin)
     {
-        _numberOfSpinWheelItems = numberOfItems;
-        _durationSpin = duration;
-        _numberRotate = numberRotate;
-        _curveSpin = curveSpin;
-        
-        StartCoroutine(SpinWheelIEnumerator());
+        var spinWheel = SpinWheel(numberOfItems, durationRotate, numberRotate, curveSpin);
+        StartCoroutine(spinWheel);
     }
 
-    private IEnumerator SpinWheelIEnumerator()
+    private IEnumerator SpinWheel(int numberOfItems, float durationRotate, int numberRotate,
+        AnimationCurve curveSpin)
     {
-        print("Çevirr");
-        yield return null;
+        spinTimer = 0;
+        var startAngle = transform.eulerAngles.z;
+
+        angleSection = SpinWheelAngle / numberOfItems;
+        var randomItem = Random.Range(1, numberOfItems);
+        var targetAngle = (numberRotate * SpinWheelAngle) + angleSection * randomItem - startAngle;
+        while (spinTimer < durationRotate)
+        {
+            yield return null;
+            spinTimer += Time.deltaTime;
+            var angleCurrent = (targetAngle) * curveSpin.Evaluate(spinTimer / durationRotate);
+            transform.eulerAngles = new Vector3(0, 0, angleCurrent + startAngle);
+        }
     }
 }
--- Assets/_GameAssets/Scripts/Controllers/Buttons/SpinButtonController.cs
+++ Assets/_GameAssets/Scripts/Controllers/Buttons/SpinButtonController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpinWheelButtonController : MonoBehaviour
+public class SpinButtonController : MonoBehaviour
 {
     #region PRIVATE PROPERTIES
 
