a04dc22ee94933dcc66a455413dc5631701d5e03
berkay <berkaykucuk1905@gmail.com>
2022-11-05 16:25:20 +0300

    wheel items prefabs

---
+++ Assets/_GameAssets/Scripts/ScriptableObjects/ItemData/WheelItemData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WheelItemData", menuName = "ItemData/WheelItemData", order = 1)]
+public class WheelItemData : ScriptableObject
+{
+    #region INSPECTOR PROPERTIES
+
+    [Range(0, 100)] [SerializeField] private float rateDrop;
+    [SerializeField] private GameObject prefabImageOnWheel;
+    [SerializeField] private GameObject prefabItemCard;
+    [SerializeField] private Sprite spriteItem;
+    #endregion
+
+    #region PUBLIC PROPERTIES
+
+    public float DropRate => rateDrop;
+    public GameObject PrefabImageOnWheel => prefabImageOnWheel;
+    public GameObject PrefabItemCard => prefabItemCard;
+
+    #endregion
+}
